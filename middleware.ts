4ad6e8e6482727d367185b153716c3ae643827b5
Alejandro Lainez <alainez420@gmail.com>
2025-08-25 19:30:40 -0500

    aro

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,18 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import jwt from "jsonwebtoken";
+import { BaseLoggger } from "html5-qrcode/esm/core";
 
 export function middleware(req: NextRequest) {
   const token = req.headers.get("authorization")?.split(" ")[1];
-
+  console.log(token, process.env)
   if (!token) {
     return NextResponse.json({ error: "No autorizado" }, { status: 401 });
   }
 
   try {
     jwt.verify(token, process.env.JWT_SECRET || "supersecret");
-    console.log(token, process.env)
+    
     return NextResponse.next();
   } catch (error) {
     return NextResponse.json({ error: "Token inválido" }, { status: 401 });
